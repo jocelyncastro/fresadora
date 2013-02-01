bc3962784edd89cd5d39c8f840b2459dabb6c66a
ucraman <dbulavka@gmail.com>
2013-02-01 06:16:00 -0300

    GraphicCNC uploaded
    Una version grafica para usar la dll, actualmente no esta funcional!!

---
+++ GraphicCNC/GraphicCNC/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GraphicCNC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboxFresado = new System.Windows.Forms.GroupBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnStep = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblComando = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboxFresado
+            // 
+            this.cboxFresado.Location = new System.Drawing.Point(394, 12);
+            this.cboxFresado.Name = "cboxFresado";
+            this.cboxFresado.Size = new System.Drawing.Size(275, 389);
+            this.cboxFresado.TabIndex = 0;
+            this.cboxFresado.TabStop = false;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(12, 12);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 1;
+            this.btnFile.Text = "Open File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // btnStep
+            // 
+            this.btnStep.Location = new System.Drawing.Point(12, 41);
+            this.btnStep.Name = "btnStep";
+            this.btnStep.Size = new System.Drawing.Size(75, 23);
+            this.btnStep.TabIndex = 2;
+            this.btnStep.Text = "Make Step";
+            this.btnStep.UseVisualStyleBackColor = true;
+            this.btnStep.Click += new System.EventHandler(this.btnStep_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 70);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // lblComando
+            // 
+            this.lblComando.AutoSize = true;
+            this.lblComando.Location = new System.Drawing.Point(12, 100);
+            this.lblComando.Name = "lblComando";
+            this.lblComando.Size = new System.Drawing.Size(85, 13);
+            this.lblComando.TabIndex = 4;
+            this.lblComando.Text = "Comando Actual";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 413);
+            this.Controls.Add(this.lblComando);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnStep);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.cboxFresado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox cboxFresado;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Button btnStep;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label lblComando;
+    }
+}
+
---
+++ GraphicCNC/GraphicCNC/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using cnc;
+using GraphicCNC.Properties;
+
+namespace GraphicCNC
+{
+    public partial class Form1 : Form
+    {
+        string[] archivo;
+        int index;
+        Main fresadora;
+        Graphics g;
+        Pen p;
+        int z;
+        float x, y;
+        public Form1()
+        {
+            InitializeComponent();
+            index = 0;
+            fresadora = new Main(
+                Settings.Default.portName,
+                Settings.Default.baudRate,
+                Settings.Default.axisXStepsPercm,
+                Settings.Default.axisYStepsPercm,
+                Settings.Default.axisZStepsPercm,
+                Settings.Default.xyMaxFeedRate,
+                Settings.Default.zMaxFeedRate
+                );
+            fresadora.makeStep += new CNCEventHandler(HandlemakeStep);
+            g = cboxFresado.CreateGraphics();
+            z = 200;
+            x = y = 0;
+        }
+
+        void HandlemakeStep(object sender, CNCEventArgs e)
+        { 
+            /*switch(e.axis)
+            {
+                case "x":
+                    if (e.avanzar)
+                        Draw(z, x+1, y);
+                    else
+                        Draw(z, x-1, y);
+                    break;
+                case "y":
+                    if (e.avanzar)
+                        Draw(z, x, y+1);
+                    else
+                        Draw(z, x, y-1);
+                    break;
+                case "z":
+                    if (e.avanzar)
+                        Draw(z + 1, x, y);
+                    else
+                        Draw(z - 1, x, y);
+                    break;
+            }*/
+        }
+
+        void Draw(int axisZ, float axisX, float axisY)
+        {
+            p = new Pen(Color.FromArgb(axisZ, Color.Black));
+            g.DrawLine(p, x, y, axisX, axisY);
+            x = axisX;
+            y = axisY;
+            z = axisZ;
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                archivo = File.ReadAllLines(openFileDialog.FileName);
+            }
+        }
+
+        void DrawLine(string xy)
+        {
+            string[] comandos = xy.Split(' ');
+            float axisX = 0;
+            float axisY = 0;
+            string temp;
+            try
+            {
+                foreach (string comando in comandos)
+                {
+                    temp = comando.Trim();
+                    temp = temp.Replace('.', ',');
+                    if (temp.Length > 1)
+                    {
+                        if (temp[0] == 'X')
+                            axisX = float.Parse(temp.Remove(0, 1));
+                        if (temp[0] == 'Y')
+                            axisY = float.Parse(temp.Remove(0, 1));
+                    }
+                }
+
+                if (axisX != 0 || axisY != 0)
+                    Draw(z, axisX*10, axisY*10);
+            }
+            catch { }
+        }
+
+        private void btnStep_Click(object sender, EventArgs e)
+        {
+            lblComando.Text = archivo[index];
+            DrawLine(archivo[index]);
+            fresadora.Handle(archivo[index++]);
+            //lblComando.Text = "Termino de procesar";
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            while(index < archivo.Length)
+            {
+                btnStep_Click(this, null);
+            }
+        }
+    }
+}
